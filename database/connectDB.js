013e06e701519cc4c50c5535de2f5a4ca60832d1
lilmeddy <aghedopriscilla@gmail.com>
2023-10-02 10:06:14 +0100

    userModel

---
+++ database/connectDB.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const uri = process.env.MONGODB_URI;
+const connectDB = async () =>{
+    try {
+        mongoose.set("strictQuery", false);
+        await mongoose.connect(uri)
+        console.log("connected");
+    } catch (error) {
+       console.log("error" + error); 
+    }
+}
+
+export { connectDB };
---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import env from "dotenv";
+import cors from "cors";
+import { connectDB } from "./database/connectDB.js";
+// import { userRoutes } from "./routes/userRoutes.js";
+// import { errorHandler } from "./middlewares/errorHandler.js";
+
+
+
+env.config();
+const app = express();
+
+app.use(cors({origin:"*"}))
+// app.use(express.json({limit:"100mb"}));
+// app.use(express.urlencoded({ extended: true, limit:"100mb"}));
+// app.use("/users", userRoutes)
+// app.use(errorHandler)
+connectDB()
+
+
+const server = app.listen(15000, ()=>{
+    
+    console.log("app is listening at port 12000");
+})
